35cab88926951498f8a37c927d1d6e95975c096b
Ballerheinz <cle.dinkel@gmail.com>
2021-04-29 15:06:30 +0200

    filters now operational, remove 'sweet'

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import Card from "./Card"
 import {Switch, Route} from 'react-router-dom';
 
 const Main = ({articles}) => {
-    /* let numArr = [1,2,3,4,5];
+    
+    let map = articles.filter(article => article.metadata.tags.map(tags => tags.sys.id).includes("sour"))
+    console.log(map)/* let numArr = [1,2,3,4,5];
      let numtimes3 = numArr.map(num => {
         console.log(num*2);
         return (num*3);
@@ -18,51 +20,65 @@ const Main = ({articles}) => {
     /* console.log(displayCards(articles)) */
 
     return (
-        <Switch>
-            <Route path="/sweet">
-                <section className="ArticleSection">
-                    { articles.map(article => 
-                        <Card article={article}/>
-                        )
-                    }
-                </section>
-            </Route>
-            <Route path="/sour">
-                <section className="ArticleSection">
-                    { articles.map(article => 
-                        <Card article={article}/>
-                        )
-                    }
-                </section>
-            </Route>
-            <Route path="/fruity">
-                <section className="ArticleSection">
-                    { articles.map(article => 
-                        <Card article={article}/>
-                        )
-                    }
-                </section>
-            </Route>
-            <Route path="/non-alcoholic">
-                <section className="ArticleSection">
-                    { articles.map(article => 
-                        <Card article={article}/>
-                        )
-                    }
-                </section>
-            </Route>
-            <Route path="/">
-                <section className="ArticleSection">
-                    { articles.map(article => 
-                        <Card article={article}/>
-                        )
-                    }
-                </section>
-            </Route>
-        </Switch>
+        <>
+            {articles ? (
+            <Switch>
+                {/* <Route path="/sweet">
+                    <section className="ArticleSection">
+                        { articles.map(article => 
+                            <Card article={article}/>
+                            )
+                        }
+                    </section>
+                </Route> */}
+                <Route path="/sour">
+                    <section className="ArticleSection">
+                        { articles
+                            .filter(article => article.metadata.tags
+                            .map(tags => tags.sys.id)
+                            .includes("sour")).map(article => 
+                            <Card article={article}/>
+                            )
+                        }
+                    </section>
+                </Route>
+                <Route path="/fruity">
+                    <section className="ArticleSection">
+                        { articles
+                            .filter(article => article.metadata.tags
+                            .map(tags => tags.sys.id)
+                            .includes("fruity")).map(article => 
+                            <Card article={article}/>
+                            )
+                        }
+                    </section>
+                </Route>
+                <Route path="/non-alcoholic">
+                    <section className="ArticleSection">
+                        { articles
+                            .filter(article => article.metadata.tags
+                            .map(tags => tags.sys.id)
+                            .includes("non-alcoholic")).map(article => 
+                            <Card article={article}/>
+                            )
+                        }
+                    </section>
+                </Route>
+                <Route path="/">
+                    <section className="ArticleSection">
+                        { articles.map(article => 
+                            <Card article={article}/>
+                            )
+                        }
+                    </section>
+                </Route>
+            </Switch>
+            ) : (<p>Loading....</p>)
+            }
+        </>
     )
 }
-
+// .filter(article => article.fields.tags.split.includes("sour"))
 //.filter(article => article.fields.tags.includes("non-alcoholic"))
 //{displayCards(articles)}
 export default Main
